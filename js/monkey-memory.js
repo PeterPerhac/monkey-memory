806f2c3fcd27da26ea4ec75f21d9412b86a09d70
Peter Perhac <perhac.peter@gmail.com>
2016-11-13 21:53:08 +0000

    full flow through all levels, in terms of circle buttons.

--- js/monkey-memory.js
+++ js/monkey-memory.js
@@ -10,26 +10,37 @@ renderer.resize(window.innerWidth, window.innerHeight);
 
 PIXI.loader
     .add("assets/images/monkey.png")
-    .load(setup);
+    .load(function () {
+        setup(0)
+    });
 
 var coin = new Howl({
     src: ['assets/sounds/coin.wav']
 });
 
 var center = {x: (renderer.width / 2), y: (renderer.height / 2)};
+var currentLevel = 0;
 
-function setup() {
-    var stage = new PIXI.Container();
+function buttonClicked() {
+    coin.play();
+    if (currentLevel == levels.length - 1) {
+        // showResultsPage();
+    } else {
+        currentLevel = currentLevel + 1;
+        setup(currentLevel);
+        // startThisLevel(currentLevel);
+    }
+}
 
-    var currentLevel = 0;
+function setup(lvl) {
 
-    var createButton = function (level) {
+    function createButton(level) {
         var btnData = buttons[levels[level].button];
         var btn = new PIXI.Graphics();
         btn.interactive = true;
         btn.lineStyle(10, btnData.color, 1);
         btn.beginFill(btnData.color, 0.3);
-        var c = {x: 0, y:0, r: btnData.radius};
+        var c = {x: 0, y: 0, r: btnData.radius};
         btn.drawCircle(c.x, c.y, c.r);
         btn.endFill();
         btn.hitArea = new PIXI.Circle(c.x, c.y, c.r);
@@ -41,15 +52,10 @@ function setup() {
         btn.position.x = center.x;
         btn.position.y = center.y;
         btn.addChild(buttonLabel);
-        btn.on('click', function () {
-            coin.play();
-        });
-        btn.on('tap', function () {
-            coin.play();
-        });
+        btn.on('click', buttonClicked);
+        btn.on('tap', buttonClicked);
         return btn;
-    };
-
+    }
     function createMessage(message, fontSize, yOffset) {
         var messageStyle = {
             fontFamily: "monospace",
@@ -66,11 +72,15 @@ function setup() {
         return msg;
     }
 
-    stage.addChild(createButton(currentLevel));
-    stage.addChild(createMessage(levels[currentLevel].message, 28, 150));
-    stage.addChild(createMessage(levels[currentLevel].subMessage, 22, 180));
+    function createLevelStartPage(level) {
+        var stage = new PIXI.Container();
+        stage.addChild(createButton(level));
+        stage.addChild(createMessage(levels[level].message, 28, 130));
+        stage.addChild(createMessage(levels[level].subMessage, 22, 170));
+        return stage;
+    }
 
-    renderer.render(stage);
+    renderer.render(createLevelStartPage(lvl));
 }
 
 // window.onresize = function () {
@@ -82,13 +92,13 @@ var buttons = [
     // 0 - start the whole thing
     {color: 0x00FF00, label: 'Start', radius: 100},
     // 1 - continue in the first part of the game
-    {color: 0xBEEF00, label: 'Next', radius: 75},
+    {color: 0x00FFFF, label: 'Next', radius: 75},
     // 2 - start the second part of the game
-    {color: 0xBEEF00, label: 'Proceed', radius: 100},
+    {color: 0x00FF00, label: 'Proceed', radius: 100},
     // 3 - continue in the second part of the game
-    {color: 0xBEEF00, label: 'Next', radius: 75},
+    {color: 0x00FFFF, label: 'Next', radius: 75},
     // 4 - end of game => show me the results
-    {color: 0xBEEF00, label: 'Results', radius: 150}
+    {color: 0xFF00FF, label: 'Results', radius: 100}
 ];
 
 var levels = [
@@ -99,31 +109,31 @@ var levels = [
         message: "Tap numbers in ascending order",
         subMessage: "Timer starts as soon as you press the button"
     },
-    {button: 1, numbers: 4, hidingNumbers: false},
-    {button: 1, numbers: 5, hidingNumbers: false},
-    {button: 1, numbers: 6, hidingNumbers: false},
-    {button: 1, numbers: 7, hidingNumbers: false},
-    {button: 1, numbers: 7, hidingNumbers: false},
-    {button: 1, numbers: 8, hidingNumbers: false},
-    {button: 1, numbers: 8, hidingNumbers: false},
-    {button: 1, numbers: 9, hidingNumbers: false},
-    {button: 1, numbers: 9, hidingNumbers: false},
-    {
-        button: 2,
-        numbers: 3,
-        hidingNumbers: true,
-        message: "Take a good look before tapping the first number",
-        subMessage: "After pressing the first number, the rest will be \"cloaked\""
-    },
-    {button: 3, numbers: 4, hidingNumbers: true},
-    {button: 3, numbers: 5, hidingNumbers: true},
-    {button: 3, numbers: 6, hidingNumbers: true},
-    {button: 3, numbers: 7, hidingNumbers: true},
-    {button: 3, numbers: 7, hidingNumbers: true},
-    {button: 3, numbers: 8, hidingNumbers: true},
-    {button: 3, numbers: 8, hidingNumbers: true},
-    {button: 3, numbers: 9, hidingNumbers: true},
-    {button: 3, numbers: 9, hidingNumbers: true},
+    // {button: 1, numbers: 4, hidingNumbers: false},
+    // {button: 1, numbers: 5, hidingNumbers: false},
+    // {button: 1, numbers: 6, hidingNumbers: false},
+    // {button: 1, numbers: 7, hidingNumbers: false},
+    // {button: 1, numbers: 7, hidingNumbers: false},
+    // {button: 1, numbers: 8, hidingNumbers: false},
+    // {button: 1, numbers: 8, hidingNumbers: false},
+    // {button: 1, numbers: 9, hidingNumbers: false},
+    // {button: 1, numbers: 9, hidingNumbers: false},
+    // {
+    //     button: 2,
+    //     numbers: 3,
+    //     hidingNumbers: true,
+    //     message: "Take a good look before tapping the first number",
+    //     subMessage: "After pressing the first number, the rest will be \"cloaked\""
+    // },
+    // {button: 3, numbers: 4, hidingNumbers: true},
+    // {button: 3, numbers: 5, hidingNumbers: true},
+    // {button: 3, numbers: 6, hidingNumbers: true},
+    // {button: 3, numbers: 7, hidingNumbers: true},
+    // {button: 3, numbers: 7, hidingNumbers: true},
+    // {button: 3, numbers: 8, hidingNumbers: true},
+    // {button: 3, numbers: 8, hidingNumbers: true},
+    // {button: 3, numbers: 9, hidingNumbers: true},
+    // {button: 3, numbers: 9, hidingNumbers: true},
     {
         button: 4,
         numbers: 0,
